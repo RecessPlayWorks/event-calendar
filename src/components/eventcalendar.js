3a5e3df7c621727a8027ca2054709bc6af570231
Beau Jackson <beau.html@gmail.com>
2019-11-27 09:43:40 -0500

    edits events

--- src/components/eventcalendar.js
+++ src/components/eventcalendar.js
@@ -4,6 +4,23 @@ import dayGridPlugin from "@fullcalendar/daygrid"
 import interactionPlugin from "@fullcalendar/interaction"
 import "@fullcalendar/core/main.css"
 import "@fullcalendar/daygrid/main.css"
+import styled from "styled-components"
+
+const StyledCalendar = styled.div`
+margin: 3vh 4vw;
+& .fc-unthemed {
+  box-shadow: 4px 4px 4px 2px rgba(0,0,0,0.5);
+  background: #81b4b4;
+  font-family: sans-serif;
+  h2 {
+    font-weight: 200;
+  }
+
+   }
+   & td.fc-event-container {
+    //  border: 2px solid white !important;
+   }
+`;
 
 class MyEventCalendar extends React.Component {
   handleEventClick = e => {
@@ -14,15 +31,27 @@ class MyEventCalendar extends React.Component {
   render() {
     const events = [
       {
-        title: "event 1",
-        start: "2019-04-02T13:13:55.008",
-        end: "2019-04-03T13:13:55.008",
-        description: "this is my event",
+        title: "Dungeons & Dragons",
+        start: "2019-11-25T16:00",
+        end: "2019-11-29T17:00",
+        description: "Well Hello!",
       },
       {
-        title: "event 2",
-        start: "2019-05-03T13:13:55.008",
-        end: "2019-05-04T13:13:55.008",
+        title: "Art Show",
+        start: "2019-11-05T15:00",
+        end: "2019-11-06T13:13:55.008",
+        description: "Well Hello!",
+      },
+      {
+        title: "Crafts Class",
+        start: "2019-11-05T16:00",
+        end: "2019-11-06T15:17:00",
+        description: "Well Hello!",
+      },
+      {
+        title: "Chess",
+        start: "2019-11-07T13:30:59.00",
+        end: "2019-11-08T14:30:00",
         description: "this is another event",
       },
       {
@@ -33,15 +62,16 @@ class MyEventCalendar extends React.Component {
       },
     ]
     return (
-      <div>
-        <h1>this is a event calendar</h1>
+      <StyledCalendar>
+        <h1>Events calendar</h1>
         <FullCalendar
+          className="full-calendar"
           eventClick={this.handleEventClick}
           defaultView="dayGridMonth"
           plugins={[dayGridPlugin, interactionPlugin]}
           events={events}
         />
-      </div>
+      </StyledCalendar>
     )
   }
 }
